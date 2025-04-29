8ab3fbf558ac1fc9cf0f7cb475406503b08321ab
Blaž <121607358+Blaz05@users.noreply.github.com>
2025-04-29 10:06:49 +0200

    Add files via upload

---
+++ Lab2/lab2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    char *p;
+
+    i quattro spazi si fanno con \t
+    
+    FILE * f;
+    printf("Dimenisone: %lu", sizeof(f));
+
+}
